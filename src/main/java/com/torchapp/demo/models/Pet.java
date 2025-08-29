c62e41f98f18cd4c3319083f6b0b093b0cb60b15
Vinícius Storch <viniciusstorch62@gmail.com>
2025-08-29 16:38:21 -0300

    Adição da classe Pet em models

---
+++ src/main/java/com/torchapp/demo/models/Pet.java
@@ -0,0 +1,36 @@
+package com.torchapp.demo.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "Pets")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String species;
+
+    @NotBlank
+    private String breed;
+
+    @NotNull
+    @Positive
+    private Double weight;
+
+    @Past
+    private LocalDate birthDate;
+}
