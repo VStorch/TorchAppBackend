026fd111f5259b2d5f7c747959843dd92ba58394
Vinícius <viniciusstorch62@gmail.com>
2025-09-24 11:23:55 -0300

    Adição de novos métodos à classe PetService

--- src/main/java/com/torchapp/demo/services/PetService.java
+++ src/main/java/com/torchapp/demo/services/PetService.java
@@ -20,4 +20,23 @@ public class PetService {
      public List<Pet> getPets() {
         return petRepository.findAll();
      }
+
+     public Pet getPetById(Long id) {
+        return petRepository.findById(id).orElseThrow(() -> new RuntimeException("Pet não encontrado"));
+     }
+
+     public Pet updatePet (Long id, Pet newPetData) {
+        return petRepository.findById(id).map(pet -> {
+            pet.setName(newPetData.getName());
+
+            return petRepository.save(pet);
+        }).orElseThrow(() -> new RuntimeException("Pet não encontrado"));
+     }
+
+     public void deletePet (Long id) {
+        if (!petRepository.existsById(id)) {
+            throw new RuntimeException("Pet não encontrado");
+        }
+        petRepository.deleteById(id);
+     }
 }
