78a6d231606de659a8f58e31f051f55ddd1a61ef
Vinícius Storch <viniciusstorch62@gmail.com>
2025-09-26 15:42:22 -0300

    Implementação de getPetById em PetController

--- src/main/java/com/torchapp/demo/controllers/PetController.java
+++ src/main/java/com/torchapp/demo/controllers/PetController.java
@@ -4,10 +4,7 @@ import com.torchapp.demo.models.Pet;
 import com.torchapp.demo.services.PetService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,16 @@ public class PetController {
     @GetMapping
     public List<Pet> getAllPets() {
         return petService.getPets();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pet> getPetById(@PathVariable Long id) {
+        try {
+            Pet pet = petService.getPetById(id);
+            return ResponseEntity.ok(pet);
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
