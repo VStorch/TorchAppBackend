182d70c636ced375f6bebb2ddb48dd02bfddebb3
Vinícius Storch <viniciusstorch62@gmail.com>
2025-09-28 15:19:58 -0300

    Bug de serialização aparentemente resolvido

--- src/main/java/com/torchapp/demo/controllers/PetController.java
+++ src/main/java/com/torchapp/demo/controllers/PetController.java
@@ -1,8 +1,8 @@
 package com.torchapp.demo.controllers;
 
 import com.torchapp.demo.dtos.pet.PetRequest;
+import com.torchapp.demo.dtos.pet.PetResponse;
 import com.torchapp.demo.models.Pet;
-import com.torchapp.demo.models.User;
 import com.torchapp.demo.services.PetService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -21,21 +21,24 @@ public class PetController {
     }
 
     @PostMapping
-    public ResponseEntity<Pet> registerPet(@Valid @RequestBody PetRequest request) {
+    public ResponseEntity<PetResponse> registerPet(@Valid @RequestBody PetRequest request) {
         Pet savedPet = petService.registerPet(request);
-        return ResponseEntity.status(201).body(savedPet);
+        return ResponseEntity.status(201).body(PetResponse.fromEntity(savedPet));
     }
 
     @GetMapping
-    public List<Pet> getAllPets() {
-        return petService.getPets();
+    public List<PetResponse> getAllPets() {
+        return petService.getPets()
+                .stream()
+                .map(PetResponse::fromEntity)
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Pet> getPetById(@PathVariable Long id) {
+    public ResponseEntity<PetResponse> getPetById(@PathVariable Long id) {
         try {
             Pet pet = petService.getPetById(id);
-            return ResponseEntity.ok(pet);
+            return ResponseEntity.ok(PetResponse.fromEntity(pet));
         }
         catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
@@ -43,10 +46,10 @@ public class PetController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Pet> updatePet(@PathVariable Long id, @RequestBody Pet pet) {
+    public ResponseEntity<PetResponse> updatePet(@PathVariable Long id, @RequestBody Pet pet) {
         try {
             Pet updatePet = petService.updatePet(id, pet);
-            return ResponseEntity.ok(updatePet);
+            return ResponseEntity.ok(PetResponse.fromEntity(updatePet));
         }
         catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
