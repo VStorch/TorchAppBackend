3f2b5db69d3f0991f0528ce6b11148340e7b6924
Vinícius Storch <viniciusstorch62@gmail.com>
2025-10-11 15:36:56 -0300

    Início da implementação de excesões costumizadas

--- src/main/java/com/torchapp/demo/exception/EntityNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.torchapp.demo.exception;
-
-public class EntityNotFoundException {
-}
---
+++ src/main/java/com/torchapp/demo/infra/RestExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.torchapp.demo.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    private ResponseEntity<String> entityNotFoundHandler (EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Entity not found");
+    }
+}
