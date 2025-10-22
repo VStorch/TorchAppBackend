f79849ce59261eb694eacac838b40b4ca4dac83e
Vinícius Storch <viniciusstorch62@gmail.com>
2025-10-22 17:41:28 -0300

    Implementação de AvailableSlotController

---
+++ src/main/java/com/torchapp/demo/dtos/slot/AvailableSlotRequest.java
@@ -0,0 +1,26 @@
+package com.torchapp.demo.dtos.slot;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+public class AvailableSlotRequest {
+
+    @NotNull(message = "Id do PetShop é um campo obrigatório.")
+    private Long petShopId;
+
+    @NotNull(message = "Data é um campo obrigatório.")
+    private LocalDate date;
+
+    @NotNull(message = "Horário de início é um campo obrigatório")
+    private LocalTime startTime;
+
+    @NotNull(message = "Horário de término é um campo obrigatório")
+    private LocalTime endTime;
+
+}
--- src/main/java/com/torchapp/demo/services/AvailableSlotService.java
+++ src/main/java/com/torchapp/demo/services/AvailableSlotService.java
@@ -1,11 +1,14 @@
 package com.torchapp.demo.services;
 
+import com.torchapp.demo.dtos.slot.AvailableSlotBulkRequest;
+import com.torchapp.demo.dtos.slot.AvailableSlotRequest;
+import com.torchapp.demo.dtos.slot.AvailableSlotResponse;
 import com.torchapp.demo.exceptions.ResourceNotFoundException;
+import com.torchapp.demo.mappers.AvailableSlotMapper;
 import com.torchapp.demo.models.AvailableSlot;
 import com.torchapp.demo.models.PetShop;
 import com.torchapp.demo.repositories.AvailableSlotRepository;
 import com.torchapp.demo.repositories.PetShopRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,24 +29,56 @@ public class AvailableSlotService {
     }
 
     @Transactional
-    public List<AvailableSlot> createSlots(Long petShopId, LocalDate date, LocalTime start, LocalTime end, int intervalMinutes) {
-        PetShop petShop = petShopRepository.findById(petShopId)
+    public AvailableSlotResponse createSlot(AvailableSlotRequest request) {
+        PetShop petShop = petShopRepository.findById(request.getPetShopId())
+                .orElseThrow(() -> new ResourceNotFoundException("PetShop não encontrado."));
+
+        AvailableSlot slot = new AvailableSlot();
+        slot.setPetShop(petShop);
+        slot.setDate(request.getDate());
+        slot.setStartTime(request.getStartTime());
+        slot.setEndTime(request.getEndTime());
+        slot.setBooked(false);
+
+        AvailableSlot saved = availableSlotRepository.save(slot);
+        return AvailableSlotMapper.toResponse(saved);
+    }
+
+    @Transactional
+    public List<AvailableSlotResponse> createSlots(AvailableSlotBulkRequest request) {
+
+        if (request.getStartTime().isAfter(request.getEndTime())) {
+            throw new IllegalArgumentException("Horário inicial não pode ser depois do final.");
+        }
+        if (request.getIntervalMinutes() <= 0) {
+            throw new IllegalArgumentException("O intervalo entre horários deve ser positivo.");
+        }
+
+        PetShop petShop = petShopRepository.findById(request.getPetShopId())
                 .orElseThrow(() -> new ResourceNotFoundException("PetShop não encontrado"));
         List<AvailableSlot> slots = new ArrayList<>();
 
-        for (LocalTime time = start; time.isBefore(end); time = time.plusMinutes(intervalMinutes)) {
+        for (LocalTime time = request.getStartTime(); time.isBefore(request.getEndTime()); time = time.plusMinutes(request.getIntervalMinutes())) {
             AvailableSlot slot = new AvailableSlot();
-            slot.setDate(date);
-            slot.setStartTime(time);
-            slot.setEndTime(time.plusMinutes(intervalMinutes));
             slot.setPetShop(petShop);
+            slot.setDate(request.getDate());
+            slot.setStartTime(time);
+            slot.setEndTime(time.plusMinutes(request.getIntervalMinutes()));
             slot.setBooked(false);
             slots.add(slot);
         }
-        return availableSlotRepository.saveAll(slots);
+        return availableSlotRepository.saveAll(slots)
+                .stream().map(AvailableSlotMapper::toResponse).toList();
     }
 
-    public List<AvailableSlot> getAvailableSlots(Long petShopId, LocalDate time) {
-        return availableSlotRepository.findByPetShopIdAndDateAndBookedFalse(petShopId, time);
+    public List<AvailableSlotResponse> getAvailableSlots(Long petShopId, LocalDate targetDate) {
+        return availableSlotRepository.findByPetShopIdAndDateAndBookedFalse(petShopId, targetDate)
+                .stream()
+                .map(AvailableSlotMapper::toResponse)
+                .toList();
+    }
+
+    public void deleteSlot(Long id) {
+        availableSlotRepository.deleteById(id);
     }
 }
