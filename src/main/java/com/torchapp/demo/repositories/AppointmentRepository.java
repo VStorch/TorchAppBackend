4ed3798325cdd179768afe53050725eaba3f294b
Vinícius Storch <viniciusstorch62@gmail.com>
2025-10-24 16:12:45 -0300

    Início da implementação de EvaluatePetShop

--- src/main/java/com/torchapp/demo/repositories/AppointmentRepository.java
+++ src/main/java/com/torchapp/demo/repositories/AppointmentRepository.java
@@ -1,7 +1,9 @@
 package com.torchapp.demo.repositories;
 
+import com.torchapp.demo.enums.AppointmentStatus;
 import com.torchapp.demo.models.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    boolean existsByUserIdAndPetShopIdAndStatus(Long userId, Long petShopId, AppointmentStatus status);
 }
---
+++ src/main/java/com/torchapp/demo/repositories/EvaluatePetShopRepository.java
@@ -0,0 +1,10 @@
+package com.torchapp.demo.repositories;
+
+import com.torchapp.demo.models.EvaluatePetShop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EvaluatePetShopRepository extends JpaRepository<EvaluatePetShop, Long> {
+    Optional<EvaluatePetShop> findByUserIdAndPetShopId(Long userId, Long petShopId);
+}
